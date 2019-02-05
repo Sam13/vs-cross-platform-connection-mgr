5dd015dc745c07695dcca7cb24eb7ee79f3deed4
Sam <merge13@gmx.net>
2019-02-05 11:00:22 +0100

    Add support to add cross platform connections

--- CrossPlatformConnectionMgr/Program.cs
+++ CrossPlatformConnectionMgr/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace CrossPlatformConnectionMgr
+﻿using System;
+using System.Security;
+using liblinux;
+using liblinux.Persistence;
+
+namespace CrossPlatformConnectionMgr
 {
 	internal sealed class Program
 	{
+		private enum Result
+		{
+			Success = 0,
+			InvalidCommandLine = 1,
+			UnsupportedCommand = 2
+		}
+
 		public static int Main(string[] args)
 		{
-			return 0;
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Invalid command line");
+				return (int) Result.InvalidCommandLine;
+			}
+
+			var command = args[0];
+
+			if (string.Compare(command, "add", StringComparison.OrdinalIgnoreCase) == 0)
+			{
+				if (args.Length < 5)
+				{
+					Console.WriteLine("Invalid command line");
+					return (int) Result.InvalidCommandLine;
+				}
+
+				var host = args[1];
+				var port = int.Parse(args[2]);
+				var user = args[3];
+				var password = args[4];
+
+				var cis = new ConnectionInfoStore();
+				var passwordSecure = new SecureString();
+				foreach (var c in password)
+				{
+					passwordSecure.AppendChar(c);
+				}
+
+				cis.Add(new PasswordConnectionInfo(host, port, TimeSpan.MaxValue, user, passwordSecure));
+				cis.Save();
+
+				return (int) Result.Success;
+			}
+			return (int) Result.UnsupportedCommand;
 		}
 	}
 }
