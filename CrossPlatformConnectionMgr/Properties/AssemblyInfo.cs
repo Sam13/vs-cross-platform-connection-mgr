3e8397c5dacd3a04b4b62424e9e31dbeee56d31e
Sam <merge13@gmx.net>
2019-02-07 13:02:38 +0100

    Add versioning with GitVersion

--- CrossPlatformConnectionMgr/Properties/AssemblyInfo.cs
+++ CrossPlatformConnectionMgr/Properties/AssemblyInfo.cs
@@ -9,5 +9,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3a1063b4-5931-4a22-bd43-a6acea34d000")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
